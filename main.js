88e3bcb6acdb8bedc114e28e0d755459f9154d90
KamlaF <100962390+KamlaF@users.noreply.github.com>
2023-03-12 22:24:36 +0100

    12/#

---
+++ main.js
@@ -0,0 +1,11 @@
+const str= 'work in progress - welcome back -';
+const text = document.getElementById("text");
+
+window.onload =()=>{
+    for(let i=0;i<str.length;i++){
+        let span = document.createElement('span');
+        span.innerText=str[i];
+        text.appendChild(span);
+        span.style.transform=`rotate(${i*11}deg)`;
+    }
+}
